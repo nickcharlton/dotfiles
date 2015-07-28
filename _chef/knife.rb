4d9016c7258d1eb16e8158885812276c7f472cca
Nick Charlton <nick@nickcharlton.net>
2015-07-28 16:19:35 +0100

    Updates the knife config.

--- _chef/knife.rb
+++ _chef/knife.rb
@@ -4,22 +4,22 @@
 # Further configuration at the repo level will override these.
 #
 
-node_name           ENV['USER']
-client_key          "#{ENV['HOME']}/.chef/#{ENV['USER']}.pem"
+node_name ENV['USER']
+client_key "#{ENV['HOME']}/.chef/#{ENV['USER']}.pem"
 
-cache_type          'BasicFile'
-cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cache_type 'BasicFile'
+cache_options(path: "#{ENV['HOME']}/.chef/checksums")
 
-cookbook_copyright  "Nick Charlton"
-cookbook_license    "apachev2"
-cookbook_email      "ops@nickcharlton.net"
+cookbook_copyright ENV['NAME']
+cookbook_email ENV['EMAIL']
+cookbook_license 'MIT'
 
 # AWS
 knife[:aws_access_key_id] = "#{ENV['AWS_ACCESS_KEY_ID']}"
 knife[:aws_secret_access_key] = "#{ENV['AWS_SECRET_ACCESS_KEY']}"
-knife[:flavor] = "t1.micro" # Micro (615MiB RAM)
-knife[:region] = "us-west-2" # Oregon
-knife[:image] = "ami-4ac9437a" # Ubuntu Precise (12.04) EBS 64bit
+knife[:flavor] = 't1.micro' # Micro (615MiB RAM)
+knife[:region] = 'eu-west-1' # Ireland
+knife[:image] = 'ami-f1810f86' # Ubuntu Trusty (14.04) EBS 64bit
 
 # Digital Ocean
 knife[:digital_ocean_client_id] = "#{ENV['DIGITAL_OCEAN_CLIENT_ID']}"
