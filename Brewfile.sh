7b53d05c44dc3ec9f7b368ba13cdc6241a3c942d
Nick Charlton <nick@nickcharlton.net>
2015-09-19 12:27:49 +0100

    Brewfile no longer works. Replaces it & tidies it up.

---
+++ Brewfile.sh
@@ -0,0 +1,28 @@
+#/usr/bin/bash
+
+# more sources
+tap homebrew/services
+brew install caskroom/cask/brew-cask
+
+# languages and runtimes
+brew install chruby ruby-install
+brew install python
+brew install go
+
+# utilities
+brew install tmux
+brew install vim
+brew install git
+brew install git-extras
+brew install tree
+brew install htop-osx
+brew install gibo
+brew install nmap
+brew install packer
+brew install redis
+brew install postgresql
+brew install jsonpp
+brew install pandoc
+
+# osx fixes
+brew install reattach-to-user-namespace
