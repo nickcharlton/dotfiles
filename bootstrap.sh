cd29c4a9be433c5523b62e36ba02414b671318ef
Nick Charlton <nick@nickcharlton.net>
2016-03-18 17:05:47 +0000

    Specify a global Ruby version.

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,6 +31,7 @@ ln -sf $DOTFILES/tmux.conf $HOME/.tmux.conf
 # ruby
 ln -sf $DOTFILES/ruby/gemrc $HOME/.gemrc
 ln -sf $DOTFILES/ruby/rubocop.yml $HOME/.rubocop.yml
+ln -sf $DOTFILES/ruby/ruby-version $HOME/.ruby-version
 
 # lldbinit
 ln -sf $DOTFILES/lldbinit $HOME/.lldbinit
