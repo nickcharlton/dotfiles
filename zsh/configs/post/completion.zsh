8f53957b513704b4ac63513c1e4909a391b7848a
Nick Charlton <nick@nickcharlton.net>
2017-11-26 16:47:00 +0000

    Match case insensitively for line completion.

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -4,3 +4,6 @@ fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 # completion
 autoload -U compinit
 compinit
+
+# matches case insensitive for lowercase
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
