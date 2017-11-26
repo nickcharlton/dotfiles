4b3136fc81ab485c48318ee03c0b3d691d6a3094
Nick Charlton <nick@nickcharlton.net>
2017-11-26 20:37:59 +0000

    Improve the prompt style and add git status.
    * Adds custom functions for setting colours right.
    * Shows the last two path components.
    * Shortens $HOME to "~".
    * Sets the current path to be "blue bold".
    * Adds [ and ] to wrap the customised block.
    * Shows the current git branch after the path.
    * Shows clean branch state as blue.
    * Shows staged/unstaged state as yellow.
    * Shows actionable state in red.
    * Shows the required action after the branch (split with a pipe).
    * Loads the git status before each command is shown.
    
    Thanks to @mandybess who inspired this setup: https://git.io/vbeUY

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,9 +1,60 @@
-# modify the prompt to contain git branch name if applicable
-git_prompt_info() {
-  current_branch=$(git current-branch 2> /dev/null)
-  if [[ -n $current_branch ]]; then
-    echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}"
-  fi
+# colours
+# Wrapped so they're reset properly and easier to read
+# Available: green, magenta, red, cyan, blue, yellow.
+prompt_color() {
+  [[ -n "$1" ]] && print "%{$fg[$2]%}$1%{$reset_color%}"
 }
-setopt promptsubst
-PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
+prompt_color_bold() {
+  [[ -n "$1" ]] && print "%{$fg_bold[$2]%}$1%{$reset_color%}"
+}
+prompt_blue()   { print "$(prompt_color "$1" blue)" }
+prompt_blue_bold() { print "$(prompt_color_bold "$1" yellow)" }
+prompt_red()    { print "$(prompt_color "$1" red)" }
+prompt_spaced() { [[ -n "$1" ]] && print " $@" }
+
+# path
+# %2~ means "show the last two components of the path, and show the home
+# directory as ~".
+#
+# Examples:
+#
+# ~/foo/bar is shown as "foo/bar"
+# ~/foo is shown as ~/foo (not /Users/nickcharlton/foo)
+prompt_shortened_path() { print "$(prompt_blue_bold "%2~")" }
+
+# git
+# see also: http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Version-Control-Information
+#
+# Sets a prompt like the following, where the `new-prompt` bit is the current
+# branch. This is blue when there's no known changes and yellow when there is.
+# When there's a current action (like a rebase), this is shown after in red.
+#
+# normal state: `[~/.dotfiles new-prompt]%`
+# when rebasing: `[~/.dotfiles new-prompt|rebase-i]%`
+autoload -Uz vcs_info
+
+local dirty_format="%F{yellow}"
+local branch_format=$(prompt_blue "%c%u%b")
+local action_status=$(prompt_red "%a")
+local action_format="${branch_format}|${action_status}"
+
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' stagedstr "${dirty_format}"
+zstyle ':vcs_info:*' unstagedstr "${dirty_format}"
+zstyle ':vcs_info:*' formats "${branch_format}"
+zstyle ':vcs_info:*' actionformats "${action_format}"
+
+prompt_git_status() {
+  # vcs_info_msg_0_ is set by the `zstyle vcs_info` directives
+  local colored_branch_name="$vcs_info_msg_0_"
+  prompt_spaced "$colored_branch_name"
+}
+
+# before every command, load the git status
+function precmd {
+  vcs_info
+}
+
+setopt prompt_subst
+PROMPT='[$(prompt_shortened_path)$(prompt_git_status)]%# '
