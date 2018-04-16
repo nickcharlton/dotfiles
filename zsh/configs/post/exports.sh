da18028f66462b2ea814f003c2dbd28af6d152ce
Nick Charlton <nick@nickcharlton.net>
2018-04-16 13:41:23 +0100

    Switch the default vagrant clone directory to ~/.
    Usually, Vagrant will create the clones relative to the current project.
    If a project happens to be in Dropbox or Time Machine, this causes
    unexpected behaviour as the files appear to be constantly changing.
    
    This switches to `~/.vagrant.d/clones` by default and ensures that it
    exists.
    
    https://www.vagrantup.com/docs/vmware/configuration.html#vm-clone-directory

--- zsh/configs/post/exports.sh
+++ zsh/configs/post/exports.sh
@@ -6,5 +6,7 @@ export DEBFULLNAME=$NAME
 export DEBEMAIL=$EMAIL
 
 export VAGRANT_DEFAULT_PROVIDER=vmware_fusion
+export VAGRANT_VMWARE_CLONE_DIRECTORY=$HOME/.vagrant.d/clones
+mkdir -p "$VAGRANT_VMWARE_CLONE_DIRECTORY"
 
 export ATLAS_USER=nickcharlton
