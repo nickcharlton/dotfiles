8111fd56b03d72e96531964d874156fe7771db9a
Nick Charlton <nick@nickcharlton.net>
2018-06-08 14:58:25 +0100

    Cache zsh completion results for 24hrs.

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -1,9 +1,17 @@
 # load our own completion functions
 fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 
-# completion
-autoload -U compinit
-compinit
+# completion; use cache if updated within 24h
+autoload -Uz compinit
+if [[ -n $HOME/.zcompdump(#qN.mh+24) ]]; then
+  compinit -d $HOME/.zcompdump;
+else
+  compinit -C;
+fi;
+
+# disable zsh bundled function mtools command mcd
+# which causes a conflict.
+compdef -d mcd
 
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
