10bfeca0bee531da979d1b8ee90a07b5bc89d2bb
Nick Charlton <nick@nickcharlton.net>
2021-09-03 12:52:06 +0100

    Add .git/safe to PATH after chruby
    Previously, `.git/safe` would always be behind `chruby`'s path because
    it used a preexec function hook through the auto loader rather than
    appending to the current value of `$PATH`. This copies the pattern
    `chruby` follows to add another function, so it's always first.

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -15,6 +15,16 @@ PATH="$GOPATH/bin:$PATH"
 PATH="$HOME/.cargo/env:$PATH"
 
 # mkdir .git/safe in the root of repositories you trust
-PATH=".git/safe/../../bin:$PATH"
+function git_safe_bin() {
+  PATH=".git/safe/../../bin:$PATH"
+}
+
+if [[ -n "$ZSH_VERSION" ]]; then
+  if [[ ! "$preexec_functions" == *git_safe_bin* ]]; then
+    preexec_functions+=("git_safe_bin")
+  fi
+elif [[ -n "$BASH_VERSION" ]]; then
+  PATH=".git/safe/../../bin:$PATH"
+fi
 
 export -U PATH
