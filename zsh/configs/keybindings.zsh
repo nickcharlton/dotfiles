b9693caae20c0e7de510cdf8bf5deb33ad90fc8b
Nick Charlton <nick@nickcharlton.net>
2023-05-16 17:05:04 +0100

    Add bindings to jump along the end of words
    This binds the conventional Alt+f (forward) Alt+b (backwards) (which
    wasn't defined before) to jump to the end, rather than start of words.
    This makes it easier to modify long commands (hi kubectl).
    
    https://superuser.com/a/1575404

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -16,3 +16,22 @@ bindkey "^N" insert-last-word
 bindkey "^Q" push-line-or-edit
 bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
 bindkey "^[[3~" delete-char
+
+# jump to the end, rather than the start of words when moving back and forth
+# for forward, this straightfoward, but we need to define a custom function to
+# go backwards
+zle -A emacs-forward-word forward-word
+
+zle -N backward-word backward-word-end
+backward-word-end() {
+  zle .backward-word
+
+  (( CURSOR )) ||
+      return
+
+  zle .backward-word
+  zle .emacs-forward-word
+}
+
+bindkey "\ef" forward-word
+bindkey "\eb" backward-word
