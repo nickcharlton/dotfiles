08c95157953e582a1cc893aa6748012eb4507e86
Nick Charlton <nick@nickcharlton.net>
2023-12-11 15:44:41 +0000

    Add OmniSharp for dotnet support
    This uses the .NET 6 (and above) native .NET, rather than configuring
    Mono. To do this, we need to configure OmniSharp and also ensure
    `DOTNET_ROOT` is set correctly for everything to work.
    
    https://github.com/OmniSharp/omnisharp-vim/issues/832
    https://stackoverflow.com/a/72225758
    https://stackoverflow.com/a/73896444

--- zsh/configs/post/exports.sh
+++ zsh/configs/post/exports.sh
@@ -22,3 +22,6 @@ export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+# dotnet
+export DOTNET_ROOT=/usr/local/share/dotnet
